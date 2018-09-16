6b6d4d866d3b2251f482e4bea841c66f4a6b464e
Kang Aan Wasi <43213343+aanwhs@users.noreply.github.com>
2018-09-16 22:48:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 ################################################################################
 # Original Author:   crombiecrunch
 # Fork Author: manfromafar
-# Current Author: Xavatar
-# Web:     
+# Previous Author: Xavatar
+# Current Author: aanwhs
+# Web: https://aanwhs.github.io 
 #
 # Program:
 #   Install yiimp on Ubuntu 17.10 running Nginx, MariaDB, and php7.1.x
